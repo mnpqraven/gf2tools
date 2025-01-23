8fca8eb47a169b6a08965616c8e3f0d9f6ae5a14
Othi <mnpq.raven@gmail.com>
2025-01-23 15:13:10 +0700

    add about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,39 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
+import { FEATURES } from "@/repository/features";
+
 export default function Page() {
-  return <div>About page</div>;
+  return (
+    <div className="flex flex-col gap-2">
+      <Card>
+        <CardHeader>
+          <CardTitle>What is this</CardTitle>
+        </CardHeader>
+        <CardContent>
+          Bunch of calculators. For suggestions/bugs encounters please contact
+          Othi#6661 on Discord.
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>TODOs</CardTitle>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-1">
+          {FEATURES.map((data) => (
+            <ListItem {...data} key={data.label} />
+          ))}
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+function ListItem({ impl: checked, label }: { impl?: boolean; label: string }) {
+  return (
+    <div className="flex items-center gap-1">
+      <Checkbox checked={checked} id={label} />
+      <Label htmlFor={label}>{label}</Label>
+    </div>
+  );
 }
---
+++ repository/features.ts
@@ -0,0 +1,8 @@
+export const FEATURES: { label: string; impl?: boolean }[] = [
+  { label: "Better doll selection" },
+  { label: "Better weapon selection" },
+  { label: "Persistent data in the browser" },
+  { label: "Phone layout" },
+  { label: "Neural helix calculator" },
+  { label: "Reorder calculating items" },
+];
