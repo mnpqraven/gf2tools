92c67934d1102980cc374387f3e0dd7e0401aefd
Othi <mnpq.raven@gmail.com>
2025-01-24 14:12:25 +0700

    feat

--- repository/features.ts
+++ repository/features.ts
@@ -5,6 +5,10 @@ export const FEATURES: { label: string; impl?: boolean }[] = [
   { label: "Phone layout" },
   { label: "Keyboard navigation support" },
   { label: "Neural helix calculator" },
+  { label: "Gacha calc" },
+  {
+    label: "Support forinput passive income sources (dispatch, boss, bp, etc)",
+  },
   { label: "Reorder calculating items" },
   { label: "Graph for resources spread" },
 ];
