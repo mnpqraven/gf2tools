6c15071f0f3062e2a064d53d26c05c3802da415a
Othi <mnpq.raven@gmail.com>
2025-03-02 19:19:52 +0700

    fix

--- lib/trpc.tsx
+++ lib/trpc.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createTRPCClient, httpBatchLink } from "@trpc/client";
 import { ReactNode, useState } from "react";
